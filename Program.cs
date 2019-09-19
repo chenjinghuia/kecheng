c34574330bb2a926077344d815d0e3a36a2a9922
shadow <shadow@LAPTOP-AFD369ML>
2019-09-19 14:48:07 +0800

    2.3.2类型推断

--- Program.cs
+++ Program.cs
@@ -6,9 +6,20 @@ namespace 开发课程
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Console.WriteLine("Hello World!");
             Console.ReadLine();
-            return;
+            return;*/
+            int d=10;
+            Console.WriteLine(d);
+            var name = "hello world";
+            var age = 25;
+            var shenme = true;
+            Type nametype = name.GetType();
+            Type agetype = age.GetType();
+            Type shenmetype = shenme.GetType();
+            Console.WriteLine("name is type:" + nametype.ToString());
+            Console.WriteLine("name is type:" + nametype.ToString());
+
         }
     }
 }
