cbb6f019aef5bf0c7b3144359f18b9efdacca638
shadow <shadow@LAPTOP-AFD369ML>
2019-09-19 15:33:04 +0800

    2.3.3变量的作用域

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 
 namespace 开发课程
 {
+    class bianliang
+    {
+        static int j = 20;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -9,7 +13,8 @@ namespace 开发课程
             /*Console.WriteLine("Hello World!");
             Console.ReadLine();
             return;*/
-            int d=10;
+
+            /*int d=10;
             Console.WriteLine(d);
             var name = "hello world";
             var age = 25;
@@ -18,7 +23,27 @@ namespace 开发课程
             Type agetype = age.GetType();
             Type shenmetype = shenme.GetType();
             Console.WriteLine("name is type:" + nametype.ToString());
-            Console.WriteLine("name is type:" + nametype.ToString());
+            Console.WriteLine("name is type:" + agetype.ToString())
+            Console.WriteLine("name is type:" + shenmetype.ToString());*/
+
+            for (int i=0;i<10;i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+            for (int i = 9;i>= 0;i--)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+            /*int j=20;
+            for(int i=0;i<10;i++)
+            {
+                int j = 30;
+                Console.WriteLine(i + j);
+            }*/
+            int j = 30;
+            Console.WriteLine(j);
 
         }
     }
