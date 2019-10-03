87b5b70c45683ea4bf778847458792fc2ec001cb
shadow <shadow@LAPTOP-AFD369ML>
2019-10-03 17:40:57 +0800

    6.8 元组

--- 课程第六章数组/Program.cs
+++ 课程第六章数组/Program.cs
@@ -197,6 +197,7 @@ namespace 课程第六章数组
         }
     }
 
+
     class Program
     {
         static void Main(string[] args)
@@ -337,8 +338,22 @@ namespace 课程第六章数组
             {
                 enumerator = enumerator.Current as IEnumerator;
             }
+
+            var result = Divide(5, 2);
+            Console.WriteLine("result of division:{0},reminder:{1}", result.Item1, result.Item2);
+
+            var tuple=Tuple.Create<string,string,string,int,int,int,double, Tuple< int, int>> ("Stephanie", "Alina", "Nagel", 2009, 6, 2, 1.37, Tuple.Create<int, int>(52, 3490));
+             Console.WriteLine(tuple.Item2);
         }
 
+
+        public static Tuple<int, int> Divide(int dividend, int divisor)
+        {
+            int result = dividend / divisor;
+            int reminder = dividend % divisor;
+
+            return Tuple.Create<int, int>(result, reminder);
+        }
 
 
         static int SumOfSqgments(ArraySegment<int>[] seqments)
