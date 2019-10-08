7aab65dde4de5a28a0eb14b5f33988f09dd27080
shadow <shadow@LAPTOP-AFD369ML>
2019-10-08 17:36:54 +0800

    8.2.5 冒泡委托实例

--- 课程第八章委托、lambda表达式和事件/Program.cs
+++ 课程第八章委托、lambda表达式和事件/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace 课程第八章委托_lambda表达式和事件
 {
@@ -54,6 +55,46 @@ namespace 课程第八章委托_lambda表达式和事件
         }
     }
     delegate double DoubleOp(double x);//定义委托
+
+    class BubbleSorter
+    {
+        static public void Sort<T>(IList<T> sortArray,Func<T,T,bool> comparison)
+        {
+            bool swapped = true;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < sortArray.Count - 1; i++)
+                {
+                    if (comparison(sortArray[i + 1], sortArray[i]))
+                    {
+                        T temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+    }
+    class Employee
+    {
+        public Employee(string name, decimal salary)
+        {
+            this.Name = name;
+            this.Salary = salary;
+        }
+        public string Name { get; private set; }
+        public decimal Salary { get; private set; }
+        public override string ToString()
+        {
+            return string.Format("{0}, {1:C}", Name, Salary);
+        }
+        public static bool CompareSalary(Employee e1, Employee e2)
+        {
+            return e1.Salary < e2.Salary;
+        }
+    }
    
     class Program
     {
@@ -89,6 +130,21 @@ namespace 课程第八章委托_lambda表达式和事件
                 ProcessAndDisplayNumber(operations[i], 7.94);
                 ProcessAndDisplayNumber(operations[i], 1.414);
                 Console.WriteLine();
+            }
+            Console.WriteLine();
+            Employee[] employees =
+            {
+            new Employee("Bugs Bunny", 20000),
+            new Employee("Elmer Fudd", 10000),
+            new Employee("Daffy Duck", 25000),
+            new Employee("Wile Coyote", 1000000.38m),
+            new Employee("Foghorn Leghorn", 23000),
+            new Employee("RoadRunner", 50000)
+            };
+            BubbleSorter.Sort(employees, Employee.CompareSalary);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
             }
         }
         static void ProcessAndDisplayNumber(DoubleOp action,double value)//把一个委托作为其第一个参数
