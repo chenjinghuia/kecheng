0a528cd20b3334ac3f31e67d00a5e88876911516
shadow <shadow@LAPTOP-AFD369ML>
2019-10-13 19:15:05 +0800

    10.11.2  BitVector32结构

--- 第10章  集合/Program.cs
+++ 第10章  集合/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -538,6 +540,7 @@ namespace 第10章__集合
 
             Console.WriteLine();
             BitArrayDemo();
+            BitVectorDemo();
         }
         static void Data_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
         {
@@ -612,6 +615,67 @@ namespace 第10章__集合
             DisplayBits(bits1);
             Console.WriteLine();//11001101 xor 10001101 = 01000000（两个输入数组只有一个数组设置为1，结果为才是1）
 
+        }
+        static void BitVectorDemo()
+        {
+            var bits1 = new BitVector32();
+            int bit1 = BitVector32.CreateMask();
+            int bit2 = BitVector32.CreateMask(bit1);
+            int bit3 = BitVector32.CreateMask(bit2);
+            int bit4 = BitVector32.CreateMask(bit3);
+            int bit5 = BitVector32.CreateMask(bit4);
+
+            bits1[bit1] = true;
+            bits1[bit2] = false;
+            bits1[bit3] = true;
+            bits1[bit4] = true;
+            Console.WriteLine(bits1);
+
+            bits1[0xabcdef] = true;
+            Console.WriteLine(bits1);
+
+
+            int received = 0x79abcdef;
+
+            var bits2 = new BitVector32(received);
+            Console.WriteLine(bits2);
+
+            // sections: FF EEE DDD CCCC BBBBBBBB AAAAAAAAAAAA
+            BitVector32.Section sectionA = BitVector32.CreateSection(0xfff);
+            BitVector32.Section sectionB = BitVector32.CreateSection(0xff, sectionA);
+            BitVector32.Section sectionC = BitVector32.CreateSection(0xf, sectionB);
+            BitVector32.Section sectionD = BitVector32.CreateSection(0x7, sectionC);
+            BitVector32.Section sectionE = BitVector32.CreateSection(0x7, sectionD);
+            BitVector32.Section sectionF = BitVector32.CreateSection(0x3, sectionE);
+
+            Console.WriteLine("Section A: " + IntToBinaryString(bits2[sectionA], true));
+            Console.WriteLine("Section B: " + IntToBinaryString(bits2[sectionB], true));
+            Console.WriteLine("Section C: " + IntToBinaryString(bits2[sectionC], true));
+            Console.WriteLine("Section D: " + IntToBinaryString(bits2[sectionD], true));
+            Console.WriteLine("Section E: " + IntToBinaryString(bits2[sectionE], true));
+            Console.WriteLine("Section F: " + IntToBinaryString(bits2[sectionF], true));
+        }
+        static string IntToBinaryString(int bits, bool removeTrailingZero)
+        {
+            var sb = new StringBuilder(32);
+
+            for (int i = 0; i < 32; i++)
+            {
+                if ((bits & 0x80000000) != 0)
+                {
+                    sb.Append("1");
+                }
+                else
+                {
+                    sb.Append("0");
+                }
+                bits = bits << 1;
+            }
+            string s = sb.ToString();
+            if (removeTrailingZero)
+                return s.TrimStart('0');
+            else
+                return s;
         }
         static void DisplayBits(BitArray bits)
         {
