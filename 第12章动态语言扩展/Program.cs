78ca4bdefc6545804e246524f3a4bb2df928ce26
shadow <shadow@LAPTOP-AFD369ML>
2019-10-26 17:02:00 +0800

    12.2 dynamic类型

---
+++ 第12章动态语言扩展/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace 第12章动态语言扩展
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GetFullName()
+        {
+            return string.Concat(FirstName, " ", LastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var staticPerson = new Person();
+            dynamic dynamicPerson = new Person();
+            //staticPerson.GetFullName("John","Smith");
+            //dynamicPerson.GetFullName("John", "Smith");  
+            Dongtai();
+            Houtai();
+        }
+        static void Dongtai()
+        {
+            dynamic dyn = 100;
+            Console.WriteLine(dyn.GetType());
+            Console.WriteLine(dyn);
+            dyn = "This is a string";
+            Console.WriteLine(dyn.GetType());
+            Console.WriteLine(dyn);
+            dyn = new Person() { FirstName = "Bugs", LastName = "Bunny" };
+            Console.WriteLine(dyn.GetType());
+            Console.WriteLine("{0} {1}",dyn.FirstName,dyn.LastName);
+        }
+        static void Houtai()
+        {
+            StaticClass staticObject = new StaticClass();
+            DynamicClass dynamicObject = new DynamicClass();
+            Console.WriteLine(staticObject.IntValue);
+            Console.WriteLine(dynamicObject.DynValue);
+
+        }
+        class StaticClass
+        {
+            public int IntValue = 100;
+        }
+        class DynamicClass
+        {
+            public dynamic DynValue = 100;
+        }
+    }
+}
---
+++ 第32章核心ADO.NET/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace 第32章核心ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string select = "UPDATE Customers " +
+                           "SET ContactName = 'Bob' " +
+                           "WHERE ContactName = 'Bill'";
+            SqlConnection conn = new SqlConnection(GetDatabaseConnection());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            int rowsReturned = cmd.ExecuteNonQuery();
+            Console.WriteLine("{0} rows returned.", rowsReturned);
+            conn.Close();
+        }
+    }
+}
