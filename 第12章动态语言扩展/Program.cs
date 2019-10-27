1ac943727a7b284b148a2d99387c483f61f1de72
shadow <shadow@LAPTOP-AFD369ML>
2019-10-27 11:06:00 +0800

    13.2 异步模式

--- 第12章动态语言扩展/Program.cs
+++ 第12章动态语言扩展/Program.cs
@@ -11,6 +11,62 @@ namespace 第12章动态语言扩展
             return string.Concat(FirstName, " ", LastName);
         }
     }
+    /*public class SearchItemResult : BindableBase
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        private string url;
+        public string Url
+        {
+            get { return url; }
+            set { SetProperty(ref url, value); }
+        }
+
+        private string thumbnailUrl;
+        public string ThumbnailUrl
+        {
+            get { return thumbnailUrl; }
+            set { SetProperty(ref thumbnailUrl, value); }
+        }
+
+        private string source;
+        public string Source
+        {
+            get { return source; }
+            set { SetProperty(ref source, value); }
+        }
+    }
+    public class SearchInfo : BindableBase
+    {
+        public SearchInfo()
+        {
+            list = new ObservableCollection<SearchItemResult>();
+            list.CollectionChanged += delegate { OnPropertyChanged("List"); };
+        }
+
+        private string searchTerm;
+
+        public string SearchTerm
+        {
+            get { return searchTerm; }
+            set { SetProperty(ref searchTerm, value); }
+        }
+
+        private ObservableCollection<SearchItemResult> list;
+        public ObservableCollection<SearchItemResult> List
+        {
+            get
+            {
+                return list;
+            }
+        }
+    }*/
     class Program
     {
         static void Main(string[] args)
